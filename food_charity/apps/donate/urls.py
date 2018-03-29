65ca432daa74ff18ed5ec74cf1febab496a898f7
Trevor Snyder <trevsny@gmail.com>
2018-03-29 14:46:43 -0700

    Trevor round 2

--- food_charity/apps/donate/urls.py
+++ food_charity/apps/donate/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^register/business$',views.register_business),
+    url(r'^register/charity$',views.register_charity),
+    url(r'^login$',views.login),
     url(r'^donor$', views.donor),
+    # url(r'^charity$', views.charity),
     url(r'^donate/home$', views.donate_render),
     url(r'^donations/all$', views.donations_all),
     url(r'^submit_donatation$', views.submit_donation) # make sure to add user.id after login is avail
--- food_charity/apps/donate/views.py
+++ food_charity/apps/donate/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect
 from models import *
 from django.contrib import messages
 from datetime import datetime
+import re
+import bcrypt
 
 def index(request):
-    request.session['id']= '1'
+    
     return render(request,'donate/index.html')
 
 def donor(request):
@@ -41,5 +43,156 @@ def submit_donation(request):
     }
     return redirect('/donate/home')
 
+def register_business(request):
+    if request.method == "POST":
+        # validations
+        state_regex = re.compile(r'^[A-Z]$')
+        email_regex = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        error = False
+        if len(request.POST['name']) < 1:
+            error = True
+            messages.error(request, "Business name field must not be empty")
+        if len(request.POST['contact']) < 1:
+            error = True
+            messages.error(request, "Contact name field must not be empty")
+        if not len(request.POST['phone']) == 10:
+            error = True
+            messages.error(request, "Phone number field must not be empty")
+        if len(request.POST['address']) < 1:
+            error = True
+            messages.error(request, "Address field must not be empty")
+        if len(request.POST['city']) < 1:
+            error = True
+            messages.error(request, "City field must not be empty")
+        if not len(request.POST['state']) == 2 and state_regex.match(request.POST['state']):
+            error = True
+            messages.error(request, "Please use state abbreviation")
+        if len(request.POST['email']) < 1:
+            error = True
+            messages.error(request, "Email field must not be empty")
+        elif not email_regex.match(request.POST['email']):
+            error = True
+            messages.error(request, "Invalid email")
+        if len(request.POST['password']) < 1:
+            error = True
+            messages.error(request, "Password must not be empty")
+        elif len(request.POST['password']) < 4:
+            error = True
+            messages.error(request, "Password must be at least 5 characters")
+        if not request.POST['password'] == request.POST['confirm_pw']:
+            error = True
+            messages.error(request, "Passwords do not match")
 
+        if error:
+            return redirect('/')
+        
+        hashed = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
+        newDonor = Donor.objects.create(business_name = request.POST['name'],contact_name = request.POST['contact'], phone_number = request.POST['phone'], email = request.POST['email'],password = hashed, admin_level = 1)
+
+        cityList = City.objects.filter(city = request.POST['city'])
+        if len(cityList) > 0:  #Check for city in DB
+            oldcity = cityList[0]
+            newDonor.city = oldcity
+        else:
+            newCity = City.objects.create(city=request.POST['city'])
+            newDonor.city = newCity
+        stateList = State.objects.filter(state = request.POST['state'])
+        if len(stateList) > 0: #Check for state in DB
+            oldState = stateList[0]
+            newDonor.state = oldState
+        else:
+            newState = State.objects.create(state=request.POST['state'])
+            newDonor.state = newState
+        newDonor.save()
+        request.session['id'] = newDonor.id
+        return redirect ('/donor')
+    else:
+        return redirect('/')
+
+def login(request):
+    if request.method == "POST":
+        donorList = Donor.objects.filter(email = request.POST['login_email'])
+        if len(donorList) > 0:
+            donor = donorList[0]
+        else:
+            messages.error(request,"Invalid credentials")
+            return redirect('/')
+
+        if bcrypt.checkpw(request.POST['login_password'].encode(),donor.password.encode()):
+            request.session['id'] = donor.id
+            return redirect('/donor')
+        else:
+            messages.error(request,"Invalid credentials")
+            return redirect('/')
+    else:
+        return redirect('/')
+    
+def register_charity(request):
+    if request.method == "POST":
+        # validations
+        email_regex = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        error = False
+        if len(request.POST['name']) < 1:
+            error = True
+            messages.error(request, "Business name field must not be empty")
+        if len(request.POST['contact']) < 1:
+            error = True
+            messages.error(request, "Contact name field must not be empty")
+        if len(request.POST['phone']) < 1:
+            error = True
+            messages.error(request, "Phone number field must not be empty")
+        if len(request.POST['address']) < 1:
+            error = True
+            messages.error(request, "Address field must not be empty")
+        if len(request.POST['city']) < 1:
+            error = True
+            messages.error(request, "City field must not be empty")
+        if not len(request.POST['state']) == 2:
+            error = True
+            messages.error(request, "Please use state abbreviation")
+        if len(request.POST['email']) < 1:
+            error = True
+            messages.error(request, "Email field must not be empty")
+        elif not email_regex.match(request.POST['email']):
+            error = True
+            messages.error(request, "Invalid email")
+        if len(request.POST['password']) < 1:
+            error = True
+            messages.error(request, "Password must not be empty")
+        elif len(request.POST['password']) < 4:
+            error = True
+            messages.error(request, "Password must be at least 5 characters")
+        if not request.POST['password'] == request.POST['confirm_pw']:
+            error = True
+            messages.error(request, "Passwords do not match")
+
+        if error:
+            return redirect('/')
+        
+        hashed = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
+        newCharity = Charity.objects.create(charity_name = request.POST['name'],contact_name = request.POST['contact'], phone_number = request.POST['phone'], email = request.POST['email'],password = hashed, admin_level = 2)
+
+        cityList = City.objects.filter(city = request.POST['city'])
+        if len(cityList) > 0:  #Check for city in DB
+            oldcity = cityList[0]
+            newCharity.city = oldcity
+        else:
+            newCity = City.objects.create(city=request.POST['city'])
+            newCharity.city = newCity
+        stateList = State.objects.filter(state = request.POST['state'])
+        if len(stateList) > 0: #Check for state in DB
+            oldState = stateList[0]
+            newCharity.state = oldState
+        else:
+            newState = State.objects.create(state=request.POST['state'])
+            newCharity.state = newState
+
+        newCharity.save()
+     
+
+       
+        request.session['id'] = newCharity.id
+        return redirect ('/charity')
+    else:
+        return redirect('/')
 
