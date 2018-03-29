ad54643085e4f0e9705212634cf39d48135956a8
Bruno Signori <signoribruno@gmail.com>
2018-03-29 15:46:38 -0700

    new comit

--- food_charity/apps/donate/urls.py
+++ food_charity/apps/donate/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^charity$', views.charity),
+    url(r'^success$', views.payment_form),
+    url(r"^checkout$", views.checkout)
 ]
--- food_charity/apps/donate/views.py
+++ food_charity/apps/donate/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render, redirect
+from . models import *
+import stripe
+
+
 
 def index(request):
+
     return render(request,'donate/index.html')
 
-# Create your views here.
+def charity(request):
+    
+    return render(request,"donate/charity.html")
+
+def payment_form(request):
+    
+   
+    
+    return render(request, "donate/success.html")
+
+def checkout(request):
+   
+
+    stripe.api_key = "pk_test_OlNSLZe6l3rBBnfUkT2KIui7"
+    
+
+    return redirect("/charity")
