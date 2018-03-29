d28303d7e7e9271aed83e24244b3853bff3cf4e1
Trevor Snyder <trevsny@gmail.com>
2018-03-29 12:47:07 -0700

    Functionality of Login and Registration

--- food_charity/apps/donate/models.py
+++ food_charity/apps/donate/models.py
@@ -1,11 +1,15 @@
 from __future__ import unicode_literals
-
+# from django.contrib.localflavor.us.forms import USStateField
 from django.db import models
 
-class Address(models.Model):
+
+class City(models.Model):
     city = models.CharField(max_length=80)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class State(models.Model):
     state = models.CharField(max_length=2)
-    zipcode = models.CharField(max_length=5)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -14,8 +18,11 @@ class Donor(models.Model):
     contact_name = models.CharField(max_length=80)
     phone_number = models.CharField(max_length=10)
     email = models.CharField(max_length=80)
-    donor_address = models.ForeignKey(Address, related_name='addresses_donor')
+    password = models.CharField(max_length = 100)
+    address = models.CharField(max_length=100,default = '')    
     admin_level = models.IntegerField()
+    city = models.ForeignKey(City, related_name="donor_city",null=True)
+    state = models.ForeignKey(State, related_name = "donor_state",null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -24,7 +31,10 @@ class Charity(models.Model):
     contact_name = models.CharField(max_length=80)
     phone_number = models.CharField(max_length=10)
     email = models.CharField(max_length=80)
-    charity_address = models.ForeignKey(Address, related_name='addresses_charity')
+    password = models.CharField(max_length = 100)
+    address = models.CharField(max_length=100,default = '')
+    city = models.ForeignKey(City, related_name="charity_city",null=True)
+    state = models.ForeignKey(State, related_name = "charity_state",null=True)
     admin_level = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- food_charity/apps/donate/urls.py
+++ food_charity/apps/donate/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^register/business$',views.register_business),
+    url(r'^register/charity$',views.register_charity),
+    url(r'^login$',views.login),
+    url(r'^donor$',views.donor),
+    url(r'^charity$', views.charity)
 ]
