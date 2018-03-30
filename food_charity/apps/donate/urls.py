cd27c91b92f6ec5ec292510dc97f93b5e8277b94
Bruno Signori <signoribruno@gmail.com>
2018-03-30 12:18:58 -0700

    final project

--- food_charity/apps/donate/urls.py
+++ food_charity/apps/donate/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
     url(r'^success$', views.payment_form),
     url(r"^checkout$", views.checkout),
     url(r'^donate/home$', views.donate_render),
-    url(r'^donor_profile/(?P<donor_id>\d+)$', views.donor_profile),
     url(r'^donations/all$', views.donations_all),
-    url(r'^submit_donatation$', views.submit_donation) # make sure to add user.id after login is avail
+    url(r'^submit_donatation$', views.submit_donation),# make sure to add user.id after login is avail
+    url(r'^add/(?P<id>\d+)$',views.add)
 ]
 # (?P<donor_id>\d+)
